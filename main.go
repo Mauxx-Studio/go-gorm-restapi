4cbba0ca9b6d1677f5446c874a366aa84c2744f9
Mauri@Note <mauxxstudio@gmail.com>
2024-11-15 14:15:55 -0300

    Se agrego el orm gorm que se conectara a una DB postgres en un contenedor docker

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/Mauxx-Studio/go-gorm-restapi/db"
 	"github.com/Mauxx-Studio/go-gorm-restapi/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	db.DBConnection()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.HomeHandler)
 
