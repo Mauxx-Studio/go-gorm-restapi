8b8f20b0d59b2386e25d878ad21f4f684e88ba55
Mauri@Note <mauxxstudio@gmail.com>
2024-11-19 15:33:43 -0300

    Agregue los modelos para las tablas y se crearon las tablas en la DB, tambien agregue un archivo notas donde estoy anotando los comandos por bash

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"net/http"
 
 	"github.com/Mauxx-Studio/go-gorm-restapi/db"
+	"github.com/Mauxx-Studio/go-gorm-restapi/models"
 	"github.com/Mauxx-Studio/go-gorm-restapi/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	db.DBConnection()
+
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.HomeHandler)
