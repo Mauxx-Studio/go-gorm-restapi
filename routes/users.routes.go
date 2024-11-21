278391535cf3c5a238e4efa069cd58f8b8de6c1b
Mauri@Note <mauxxstudio@gmail.com>
2024-11-21 13:31:06 -0300

    Agregue las funciones de agrergar usuario y obtener usuarios

--- routes/users.routes.go
+++ routes/users.routes.go
@@ -1,1 +1,38 @@
 package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Mauxx-Studio/go-gorm-restapi/db"
+	"github.com/Mauxx-Studio/go-gorm-restapi/models"
+)
+
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	var users []models.User
+	db.DB.Find(&users)
+	json.NewEncoder(w).Encode(&users)
+}
+
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Get User"))
+}
+
+func PostUserHandler(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+
+	json.NewDecoder(r.Body).Decode(&user)
+
+	createdUser := db.DB.Create(&user)
+	err := createdUser.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // codigo 400
+		w.Write([]byte(err.Error()))
+	}
+
+	json.NewEncoder(w).Encode(&user)
+}
+
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Delete"))
+}
